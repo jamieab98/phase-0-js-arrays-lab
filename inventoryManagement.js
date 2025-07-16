df5e9a796cb39ad3bd54f2d1c2bdbbedaa52a815
Jamie Bryson <jamieab98@gmail.com>
2025-07-16 18:12:31 -0400

    Starting to make the Add a Product function

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,12 +1,26 @@
 // Write your code here
+const products = [];
+products.push("Laptop", "Phone", "Headphones", "Monitor")
+
+function logFirstProduct () {
+  console.log(products[0]);
+}
+logFirstProduct()
+
+products.push("Computer")
+console.log(products)
+
+//function addProduct("newProduct"){
+//  products.push("newProduct");
+//}
 
 
 
 // Export the necessary parts for testing
-module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
-};
+//module.exports = {
+//  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
+//  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
+//  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
+//  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
+//  products
+//};
