332e5ee08a4ee9df0e1d7461c473ab6265236df9
Jamie Bryson <jamieab98@gmail.com>
2025-07-17 17:10:49 -0400

    finished the assignment

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -20,13 +20,17 @@ function updateProductName(position, newName) {
 }
 updateProductName(1, "macBook")
 
-
+function removeLastProduct() {
+  products.pop();
+  console.log(products);
+}
+removeLastProduct()
 
 // Export the necessary parts for testing
-//module.exports = {
-//  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-//  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-//  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-//  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-//  products
-//};
+module.exports = {
+  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
+  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
+  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
+  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
+  products
+};
